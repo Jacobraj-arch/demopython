ea7e9f33be2b414dc620428052c21e6318d39fe8
Jacobraj-arch <jacobraj2019@gmail.com>
2025-04-02 11:21:54 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -3,3 +3,8 @@ num2 = float(input("Enter Second Number: "))
 
 print(f"sum: {num1 + num2}")
 print(f"sum: {num1 + num2}")
+
+num1 = float(input("Enter first Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print(f"sum: {num1 + num2}")
