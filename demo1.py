4d065821b8e8e72dc00e6449d4763c5abd552b8a
Jacobraj-arch <jacobraj2019@gmail.com>
2025-04-02 10:37:41 +0530

    demo1

--- demo1.py
+++ demo1.py
@@ -1,1 +1,15 @@
+num1 = float(input("Enter first Number: "))
+num2 = float(input("Enter Second Number: "))
 
+print(f"sum: {num1 + num2}")
+print(f"sub: {num1 - num2}")
+print(f"sub: {num1 - num2}")
+
+
+
+um1 = float(input("Enter first Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print(f"sum: {num1 + num2}")
+print(f"sub: {num1 - num2}")
+print(f"sub: {num1 - num2}")
